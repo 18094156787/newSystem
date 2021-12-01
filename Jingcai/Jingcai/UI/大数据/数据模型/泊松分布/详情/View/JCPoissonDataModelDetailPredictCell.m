8d513f5923c2b65aa41648baa41943c14029c916
陈继伟 <549281042@qq.com>
2021-12-01 15:19:59 +0800

    no message

--- Jingcai/Jingcai/UI/大数据/数据模型/泊松分布/详情/View/JCPoissonDataModelDetailPredictCell.m
+++ Jingcai/Jingcai/UI/大数据/数据模型/泊松分布/详情/View/JCPoissonDataModelDetailPredictCell.m
@@ -22,27 +22,42 @@
             
             NSInteger num = j+6*i;
 //            if (self.dataArray.count>num) {
+            UIView *bottomView = [UIView new];
+            bottomView.layer.borderWidth = 0.5;
+            bottomView.layer.borderColor = COLOR_E8E8E8.CGColor;
+            bottomView.frame = CGRectMake(AUTO(15)+width*j, height*i, width, height);
+            [self.contentView addSubview:bottomView];
                 
-                UILabel *titleLab = [UILabel initWithTitle:@"" andFont:AUTO(12) andWeight:1 andTextColor:COLOR_2F2F2F andBackgroundColor:JCClearColor andTextAlignment:NSTextAlignmentCenter];
-                titleLab.layer.borderWidth = 0.5;
-                titleLab.layer.borderColor = COLOR_E8E8E8.CGColor;
-                titleLab.numberOfLines = 2;
-                titleLab.frame = CGRectMake(AUTO(15)+width*j, height*i, width, height);
+                UILabel *titleLab = [UILabel initWithTitle:@"" andFont:AUTO(14) andWeight:2 andTextColor:COLOR_2F2F2F andBackgroundColor:JCClearColor andTextAlignment:NSTextAlignmentCenter];
+//                titleLab.layer.borderWidth = 0.5;
+//                titleLab.layer.borderColor = COLOR_E8E8E8.CGColor;
+//                titleLab.numberOfLines = 2;
+                titleLab.frame = CGRectMake(AUTO(15)+width*j, AUTO(4)+height*i, width, AUTO(20));
                 [self.contentView addSubview:titleLab];
+                UILabel *bottomLab = [UILabel initWithTitle:@"" andFont:AUTO(12) andWeight:1 andTextColor:COLOR_2F2F2F andBackgroundColor:JCClearColor andTextAlignment:NSTextAlignmentCenter];
+//                bottomLab.layer.borderWidth = 0.5;
+//                bottomLab.layer.borderColor = COLOR_E8E8E8.CGColor;
+                bottomLab.frame = CGRectMake(AUTO(15)+width*j, AUTO(24)+height*i, width, AUTO(16));
+                [self.contentView addSubview:bottomLab];
             if (self.dataArray.count>num) {
                 NSArray *array = self.dataArray[num];
                     if (array.count==2) {
                         NSString *value1 = array.firstObject;
                         NSString *value2 = array.lastObject;
-                        titleLab.text = [NSString stringWithFormat:@"%@\n%@",value1,value2];
+                        titleLab.text = value1;
+                        bottomLab.text = value2;
+//                        titleLab.text = [NSString stringWithFormat:@"%@\n%@",value1,value2];
                     }else if(array.count==1){
                         NSString *value1 = array.firstObject;
                         titleLab.text = value1;
+                        bottomLab.text = @"";
                     }else {
                         titleLab.text = @"";
+                        bottomLab.text = @"";
                     }
             }else{
                 titleLab.text = @"";
+                bottomLab.text = @"";
             }
 
 
